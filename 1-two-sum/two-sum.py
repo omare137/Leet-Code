b0cc9807ee1e2e78b1029530df6dcc854d366296
omare137 <125502213+omare137@users.noreply.github.com>
2026-07-18 04:28:36 +0400

    Time: 0 ms (100.00%) | Memory: 19.8 MB (81.86%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        left, right = 0, len(nums) - 1
+        n=nums
+        sortedNums=sorted(n)
+        while left<right:
+            cur = sortedNums[left] + sortedNums[right]
+            if cur == target:
+                break
+            elif cur<target:
+                left +=1
+            else:
+                right-=1
+    
+        left=n.index(sortedNums[left])
+        n[left]=1.67
+        right=n.index(sortedNums[right])
+        return (left, right)
+
+        
+
+
+
+        
